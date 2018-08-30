f78ff6cb6c874ca75fd2265fd4167bd1e69dd0e9
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2018-08-30 09:59:34 -0400

    WeaponThrow

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+    public enum States
+    {
+        Idle,Attack,Jump,Hit,Death,Switch
+    }
+
+    public States CurrentState = States.Idle;
+	void Start () {
+
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Z)){
+            
+            anim.SetTrigger ("Attack");
+           
+		}
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            //Throw the weapon
+
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            CurrentState = States.Attack;
+           
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+           
+            CurrentState = States.Idle;
+        }
+		
+        
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "Enemy")
+        {
+            if(CurrentState == States.Attack)
+            {
+                Debug.Log("Enemy hit : " + collision.gameObject.name + " takes damage from player");
+            }
+        }
+      
+    }
+}
---
+++ Assets/Scripts/WeaponThrow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponThrow : MonoBehaviour {
+
+	// Use this for initialization
+
+        //play with x value
+
+    public enum State
+    {
+        Idle,Throw,Return
+    }
+
+    public State Current;
+
+
+	void Start () {
+
+        Current = State.Idle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Current == State.Throw)
+        {
+            this.transform.Translate(new Vector2(2 * Time.deltaTime, 0));
+        }
+
+        if(Current == State.Return)
+        {
+           this.transform.position =  this.transform.parent.position;
+        }
+	}
+}
